468c3270012d8d14c694696c8478f9370a80326f
Edgar Corona <coronam.edgar@gmail.com>
2022-07-21 18:36:47 -0500

    Dots Animation

--- js/script.js
+++ js/script.js
@@ -9,16 +9,34 @@ $(document).ready(function() {
     $('[id$=Animation]').one('click', function(event) {
         $("#"+this.id).removeClass("animate__infinite");
     });
-
-    for (let i = 0; i < 36; i++) {
     
+    //getDots();
+    function getDots(){
+        let style;
+        let animationClass = "animate__animated animate__backInDown";
+        $("#puntitos").empty();
+        for (let i = 1; i < 100; i++) {
+            style = "position: absolute;"+
+            "z-index: -1;"+
+            "left: "+getRandom(0,$("html").width()-10)+"px;"+
+            "top: "+getRandom(0,$("html").height())+"px;"+
+            "background: #9D7587;"+
+            "height: "+getRandom(5,10)+"px;"+
+            "width: "+getRandom(5,10)+"px;"+
+            "border-radius: 5px;"+
+            "transform: rotate3d(1, 1, 1, "+getRandom(15,45)+"deg);"+
+            "--animate-duration: 10s;"+
+            "--animate-delay: "+getRandom(0,1)+"s;"
+            $("#puntitos").append("<div style='"+style+"' class='"+animationClass+"'></div>");
+        }
     }
+    
 
     function getRandom(min, max) {
         return (Math.random() * (max - min ) + min); 
     }
     function updateScreenSize(){
-
+        getDots();
     }
 
     $('#addressAnimation').click( ()=> {
